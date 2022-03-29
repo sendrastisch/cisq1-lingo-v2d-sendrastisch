f1845f37a7019d84ab64abddb14fa7e7c7c1fdd0
sendrastisch <sandra.fernandes@student.hu.nl>
2022-03-29 21:59:37 +0200

    add NoGamesFoundException

---
+++ src/main/java/nl/hu/cisq1/lingo/Game/application/exceptions/NoGamesFoundException.java
@@ -0,0 +1,5 @@
+package nl.hu.cisq1.lingo.Game.application.exceptions;
+
+public class NoGamesFoundException extends RuntimeException{
+    public NoGamesFoundException(String message) {super(message);}
+}
